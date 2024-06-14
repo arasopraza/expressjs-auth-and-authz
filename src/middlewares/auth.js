8a1573eb3b39adfe03ca4b4a470afe60dc148dfa
arasopraza <aras.opraza@gmail.com>
2024-06-14 11:16:06 +0700

    fix: middlewares jwt

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,19 @@
 const TokenManager = require('../services/security/JsonWebToken');
+const response = require('../utils/response');
 
-const authenticateJWT = (req, res, next) => {
+async function authenticateJWT (req, res, next) {
   const token = req.headers.authorization;
   if (token && token.indexOf('Bearer ') !== -1) {
     try {
-      const user = TokenManager.verify(token.split('Bearer ')[1], process.env.JWT_SECRET);
+      const user = await TokenManager.verify(token.split('Bearer ')[1], process.env.JWT_SECRET);
       req.user = user;
       return next();
     } catch (error) {
-      res.sendStatus(403);
+      return response(res, 401, error.message, null);
     }
   }
 
-  res.sendStatus(401);
+  return response(res, 401, 'Unauthorized', null);
 };
 
 module.exports = authenticateJWT;
